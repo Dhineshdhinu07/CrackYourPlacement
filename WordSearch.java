39d56959a90a9350b26a044422e32ca7d9d95582
Dhineshdhinu07 <dhinesh1278@gmail.com>
2024-07-18 21:20:41 +0530

    Problem - 17

---
+++ WordSearch.java
@@ -0,0 +1,38 @@
+public class WordSearch {
+    public static void main(String[] args) {
+        char[][] board = {
+            {'A','B','C','E'},
+            {'S','F','C','S'},
+            {'A','D','E','E'},
+        };
+        String word = "ABCCED";
+        System.out.println(exist(board, word));
+    }
+    public static boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; ++i)
+          for (int j = 0; j < board[0].length; ++j)
+            if (dfs(board, word, i, j, 0))
+              return true;
+        return false;
+      }
+    
+      private static boolean dfs(char[][] board, String word, int i, int j, int s) {
+        if (i < 0 || i == board.length || j < 0 || j == board[0].length)
+          return false;
+        if (board[i][j] != word.charAt(s) || board[i][j] == '*')
+          return false;
+        if (s == word.length() - 1)
+          return true;
+    
+        char cache = board[i][j];
+        board[i][j] = '*';
+        boolean isExist = dfs(board, word, i + 1, j, s + 1) || //
+                            dfs(board, word, i - 1, j, s + 1) || //
+                            dfs(board, word, i, j + 1, s + 1) || //
+                            dfs(board, word, i, j - 1, s + 1);
+        board[i][j] = cache;
+    
+        return isExist;
+      }
+    
+}
